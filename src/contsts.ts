c14131e38932a0898bd1c9a564024cbf84baeb22
Jan Bulling <jan.greenengage@gmail.com>
2024-12-22 15:41:26 +0100

    initial progress and design

---
+++ src/contsts.ts
@@ -0,0 +1,46 @@
+export const site = {
+  title: "Jan Bulling",
+  description: "Personal portfolio of Jan Bulling",
+  author: "Jan Bulling",
+};
+
+// Links
+export const links = [
+  {
+    text: "Home",
+    href: "/",
+  },
+  {
+    text: "Experiences",
+    href: "/experiences",
+  },
+  {
+    text: "Projects",
+    href: "/projects",
+  },
+];
+
+// Socials
+type Socials = {
+  name: string;
+  text: string;
+  href: string;
+}[];
+
+export const socials: Socials = [
+  {
+    name: "Email",
+    text: "jan.bulling@gmx.de",
+    href: "mailto:jan.bulling@gmx.de",
+  },
+  {
+    name: "Github",
+    text: "janbulling",
+    href: "https://github.com/janbulling",
+  },
+  {
+    name: "LinkedIn",
+    text: "Jan Bulling",
+    href: "https://www.linkedin.com/in/jan-bulling/",
+  },
+];
